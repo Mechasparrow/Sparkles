012b7b698c644cb958e71d67a89d5f4b0b8ee885
Michael Navazhylau <mikipux7@gmail.com>
2017-10-23 19:01:30 -0500

    made changes

---
+++ client.py
@@ -0,0 +1,10 @@
+import websocket
+from websocket import create_connection
+
+ws = create_connection("ws://sparkle-coin-server.herokuapp.com/echo")
+
+ws.send("hello there")
+
+result = ws.recv()
+
+print (result)
