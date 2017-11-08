b7717be148ce565e1b0d55a73ba5751f1d2f9384
Qubit <mikipux7@gmail.com>
2017-11-08 09:11:09 -0600

    working on the wallet client right now

--- node/node_client.py
+++
@@ -1,7 +0,0 @@
-import websocket
-from websocket import create_connection
-import time
-
-ws = create_connection("ws://localhost:5000/miners")
-ws.send("yo")
-ws.close()
---
+++ node/wallet_client.py
@@ -0,0 +1,43 @@
+import websocket
+import _thread
+import time
+
+def on_message(ws, message):
+
+    pass
+
+def on_error(ws, error):
+    print (error)
+
+def on_close(ws):
+    print ("### closed ###")
+
+def on_open(ws):
+    print ("### open ###")
+
+    def run (*args):
+        while (True):
+            mode = input("What would you like to do? (transaction, balance, exit): ")
+
+            if (mode == "exit"):
+                ws.send("wallet disconnect")
+                ws.close()
+                break
+            elif (mode == "transaction"):
+                print ("beginning transaction...")
+                amnt = input("How much would you like to send?: ")
+
+        print ("thread terminating")
+
+    _thread.start_new_thread(run, ())
+
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("ws://localhost:5000/wallet",
+        on_message = on_message,
+        on_error = on_error,
+        on_close = on_close
+    )
+
+    ws.on_open = on_open
+    ws.run_forever()
